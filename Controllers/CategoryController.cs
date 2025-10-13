9be3dd4949e30b761b4a92b8d6bd49b421d527cb
Mosa <Mohamedmosa6000@gmail.com>
2025-10-13 16:22:29 +0300

    Add Delete , Edit Action

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -24,11 +24,12 @@ namespace ClickSouq.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(Category Obj)
         {
-            if (Obj.Name == Obj.DisplayOrder.ToString()) 
+            if (Obj.Name == Obj.DisplayOrder.ToString())
             {
-                ModelState.AddModelError("Name","The Display Order Cannot exactly match the Name. ");
+                ModelState.AddModelError("Name", "The Display Order Cannot exactly match the Name. ");
             }
             if (ModelState.IsValid)
             {
@@ -38,5 +39,63 @@ namespace ClickSouq.Controllers
             }
             return View(Obj);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _context.categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category Obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.categories.Update(Obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _context.categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var category = _context.categories.Find(id);
+
+            if (category == null)
+                return NotFound();
+
+            _context.categories.Remove(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+
     }
 }
