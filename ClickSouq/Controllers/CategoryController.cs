80b491e21a71596e8fceb0b58fd0a59d24ac3c04
Mosa <Mohamedmosa6000@gmail.com>
2025-10-15 21:53:50 +0300

    add classes library and add some frontend

---
+++ ClickSouq/Controllers/CategoryController.cs
@@ -0,0 +1,106 @@
+﻿using ClickSouq.Data;
+using ClickSouq.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClickSouq.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var categories = _context.categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category Obj)
+        {
+            if (Obj.Name == Obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display Order Cannot exactly match the Name. ");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.categories.Add(Obj);
+                _context.SaveChanges();
+                TempData["success"] = "Category Created successfully";
+                return RedirectToAction("Index");
+            }
+           
+            return View(Obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _context.categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category Obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.categories.Update(Obj);
+                _context.SaveChanges(); 
+                TempData["success"] = "Category Edit successfully";
+                return RedirectToAction("Index");
+            }
+           
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _context.categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var category = _context.categories.Find(id);
+
+            if (category == null)
+                return NotFound();
+
+            _context.categories.Remove(category);
+            _context.SaveChanges();
+            TempData["success"] = "Category Delete successfully";
+            return RedirectToAction("Index");
+
+        }
+
+
+    }
+}
