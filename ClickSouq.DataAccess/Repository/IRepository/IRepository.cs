cd8acf444c844ec9b66da764aa982e52f689df4c
Mosa <Mohamedmosa6000@gmail.com>
2025-10-17 00:16:47 +0300

    Add Class Repository implement the interFace IRepository

--- ClickSouq.DataAccess/Repository/IRepository/IRepository.cs
+++ ClickSouq.DataAccess/Repository/IRepository/IRepository.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClickSouq.DataAccess.Repository.IRepository
 {
-    internal interface IRepository
+    public interface IRepository<T> where T : class
     {
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T item);
+        void Remove(T item);
+        void RemoveRange(IEnumerable<T> Items);
+
     }
 }
---
+++ ClickSouq.DataAccess/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using ClickSouq.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClickSouq.DataAccess.Repository
+{
+    internal class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> Dbset;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            Dbset = _db.Set<T>();
+          // Dbset = _db.Categories
+        }
+        public void Add(T item)
+        {
+           Dbset.Add(item);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = Dbset;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+          
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = Dbset;
+            return query.ToList();
+        }
+
+        public void Remove(T item)
+        {
+            Dbset.Remove(item);
+        }
+
+        public void RemoveRange(IEnumerable<T> Items)
+        {
+            Dbset.AddRange(Items);
+        }
+    }
+}
