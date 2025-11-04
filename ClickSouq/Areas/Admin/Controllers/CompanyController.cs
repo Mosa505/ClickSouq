ea29329715de84e5c02b264038b0c22799556256
Mosa <Mohamedmosa6000@gmail.com>
2025-11-04 15:11:01 +0200

    Add Company Controller And View In Area Admin

---
+++ ClickSouq/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,26 @@
+﻿using BookNest.DataAccess.Repository.IRepository;
+using BookNest.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookNest.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Company)]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var com = _unitOfWork.Company.GetAll();
+            return View(com);
+
+        }
+    }
+}
