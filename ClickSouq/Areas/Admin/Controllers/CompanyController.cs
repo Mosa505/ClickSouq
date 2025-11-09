a4e0ae7fd69cf50c9897d1ed5b74e18b74ccc709
Mosa <Mohamedmosa6000@gmail.com>
2025-11-09 21:39:37 +0200

    add Upsert action and view

--- ClickSouq/Areas/Admin/Controllers/CompanyController.cs
+++ ClickSouq/Areas/Admin/Controllers/CompanyController.cs
@@ -1,8 +1,11 @@
 ﻿using BookNest.DataAccess.Repository.IRepository;
+using BookNest.Models;
+using BookNest.Models.ViewModels;
 using BookNest.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BookNest.Areas.Admin.Controllers
 {
@@ -24,10 +27,29 @@ namespace BookNest.Areas.Admin.Controllers
         }
 
         [HttpGet]
+        public IActionResult Upsert(int? id)
+        {
+            var Empty_company = new Company();
+             
+            if (id == null || id == 0)
+            {
+                return View(Empty_company);
+            }
+            else
+            {var company = _unitOfWork.Product.Get(e => e.Id == id);
+                return View(company);
+            }
+
+        }
+
+
+        #region Call API
+        [HttpGet]
         public IActionResult GetAll()
         {
             var AllCompany = _unitOfWork.Company.GetAll().ToList();
             return Json(new { data = AllCompany });
-        }
+        } 
+        #endregion
     }
 }
