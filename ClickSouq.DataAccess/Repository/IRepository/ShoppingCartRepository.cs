1898a896211c4125ef4177598dd06ab3a23100e9
Mosa <Mohamedmosa6000@gmail.com>
2025-11-23 21:51:30 +0200

    Add Shopping Cart Repo

---
+++ ClickSouq.DataAccess/Repository/IRepository/ShoppingCartRepository.cs
@@ -0,0 +1,25 @@
+﻿using BookNest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookNest.DataAccess.Repository.IRepository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository (ApplicationDbContext db) :base(db)
+        {
+            _db = db;
+        }
+        
+
+        public void Update(Category category)
+        {
+            _db.Update(category);
+        }
+    }
+}
